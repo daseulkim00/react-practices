3607f7aa4b685cf02e12d77f30b7ded0b2ba3070
daseulkim00 <ektmf8619@naver.com>
2021-11-23 10:27:28 +0900

    webpack 연습#1

--- 01.basic/webpack-practices/ex06/webpack.config.js
+++ 01.basic/webpack-practices/ex06/webpack.config.js
@@ -5,25 +5,25 @@ module.exports = {
     entry: path.resolve('src/index.js'),
     output:{
         path: path.resolve('public'),
-        filename: 'bundle.js',
-        assetsModuleFileName: 'assets/images/[hash][ext]'
+        filename: 'bundle.js'
     },
     module: {
         rules:[{
             test: /\.(sa|sc|c)ss$/i,
             use:['style-loader' , 'css-loader', 'sass-loader']   // 순서가 중요함
-        }
-    
-    ]
+        },{
+            test: /\.(png|gif|jpe?g|svg|ico|tiff?|bmp)$/i,   //jpg 도되고 jpge도되고
+            type: 'asset/resource'
+        }]
     },
+    devtool:"eval-source-map", // 번들링이랑 우리 원래소스랑 연결시켜주는 설정?
     devServer:{
-        // contentBase: path.resolve('public'),
         host:'0.0.0.0',
         port:9999,
-        // inline:true,
         liveReload: true,
         hot: false,
-        compress:true
+        compress:true,
+        historyApiFallback:true
 
     }
 }
